478baa3b9627e4679d8425a8ae8eb5178fae07a3
Tejas Agarwal <tejasagarwal@Tejass-Laptop-2.local>
2025-07-05 17:01:54 +0530

    Add model script

---
+++ model.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+import joblib
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train[..., tf.newaxis] / 255.0
+x_test = x_test[..., tf.newaxis] / 255.0
+
+model = Sequential([
+    #Block 1
+    Conv2D(32, (3,3), activation='relu', padding='same', input_shape=(28, 28, 1)),
+    BatchNormalization(),
+    MaxPooling2D((2,2)),
+    #Block 2
+    Conv2D(64, (3,3), activation='relu', padding='same', input_shape=(28, 28, 1)),
+    BatchNormalization(),
+    MaxPooling2D((2,2)),
+    #Block 3
+    Conv2D(128, (3,3), activation='relu', padding='same', input_shape=(28, 28, 1)),
+    BatchNormalization(),
+    MaxPooling2D((2,2)),
+    #Block 4
+    Conv2D(256, (3,3), activation='relu', padding='same', input_shape=(28, 28, 1)),
+    BatchNormalization(),
+
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dropout(0.5),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5, validation_split=0.1)
+
+model.save("mnist_cnn.h5")
